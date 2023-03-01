a27bf019e4bfaf10c4561d8f7f58195bf0495a35
Gael Mukendi Kabongo <111666680+gmka98@users.noreply.github.com>
2023-03-01 04:30:37 +0100

    Update game.py

--- game.py
+++ game.py
@@ -6,46 +6,31 @@ class Action(IntEnum):
     Paper = 1
     Scissors = 2
 
+possible_actions = ["rock", "paper", "scissors"]
 
 while True:
-    user = input ('Make a choice (ROCK,PAPER,SCISSORS):')
-    possible_ations = ["rock,paper,scissors"]
-    IA_action = random.choice(possible_ations)
-    print(f"\n You chose {user}, It chose {IA_action}.\n")
-    
+    user = input('Make a choice (ROCK,PAPER,SCISSORS): ').upper()
+    IA_action = random.choice(possible_actions)
+    print(f"\nYou chose {user}, It chose {IA_action}.\n")
+
     if user == IA_action:
         print("It's a tie nobody won :| both of you lose")
-        
     elif user == 'ROCK':
-    
-     if IA_action == "scissors":
-        print('THE ROCK ! Bottom the scissors!!! YOU WIN! >:(')
-    else:
-        print('Paper recover The Rock! You loose :)')
-        
-    if user == "PAPER":
-    
-    elif IA_action == "scissors":
-    print("The Paper lose again the scissors >:), You loose")
-    
-else:("THE ROCK ! recover the paper with his people CHAMP!! :( You win!")
+        if IA_action == "scissors":
+            print('THE ROCK! Bottoms the scissors!!! YOU WIN! >:(')
+        else:
+            print('Paper recovers the rock! You lose :)')
+    elif user == "PAPER":
+        if IA_action == "scissors":
+            print("The paper loses against the scissors >:), you lose")
+        else:
+            print("The rock recovers the paper with his people CHAMP!! :( You win!")
+    elif user == "SCISSORS":
+        if IA_action == "paper":
+            print("THE SCISSORS like your cut G! :/ You Win!")
+        else:
+            print("Rock smashes your scissors hahaha ;) You LOSE!")
 
-     elif user == "SCISSORS":
-    if IA_action == "Paper":
-        print("THE SCiSSors like your cut G! :/ You Win!")
-else:
-        print("rock smashes your scissors hahaha ;) You LOOSE!")
-
-        play_again = input("Lose again?" "(y/n): ")
-        if play_again.lower() != "y":
-            break
-
->>> Action.Rock == Action(0)
-True
->>> Action(0)
-<Action.Rock: 0>
-    function finale(){
-    }
-
-
-    
+    play_again = input("Play again? (y/n): ")
+    if play_again.lower() != "y":
+        break
